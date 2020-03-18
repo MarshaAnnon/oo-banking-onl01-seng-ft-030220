582a0abb898d294d9e14796f77ec2a3ea6082dd3
Marsha Annon <marshaannon@gmail.com>
2020-03-18 12:53:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+
+attr_accessor :sender, :bank_transfer
+
+  def initialize(bank_transfer, sender, receiver)
+    @bank_transfer = bank_transfer
+    @sender = sender
+
+  end
+
 end
