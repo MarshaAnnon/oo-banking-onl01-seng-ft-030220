b59137a321e5c9b846cf13be876fa2ee9b0a9c0d
Marsha Annon <marshaannon@gmail.com>
2020-03-18 13:56:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ attr_accessor :sender, :receiver, :status, :amount
   def valid?
     if @sender = true && @receiver = true
        @bank_transfer = true
-     
+
     end
     @bank_transfer.valid?
   end
