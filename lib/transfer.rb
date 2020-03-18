7c7ee7d0c02c6a395a9927f791eb41ff3c13a211
Marsha Annon <marshaannon@gmail.com>
2020-03-18 14:37:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,14 @@ attr_accessor :sender, :receiver, :status, :amount
   end
 
   def execute_transaction
-
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end
 
 end
